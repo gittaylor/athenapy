5fb0ab50ea84b63a77b7dc2e64c80f31122ed05c
Taylor Sittler <taylor.sittler@gmail.com>
2013-01-10 10:49:25 -0800

    forgot to add unittests file

---
+++ tests.py
@@ -0,0 +1,28 @@
+import unittest
+import convert
+from os import remove
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.seq = range(10)
+
+    def test_convertSalesforceToSFMR(self):
+        sd = convert.SurveyData()
+        datadictCSV = 'testdata/Athena questionnaire data dictionary - Breast Health History Intake Form.csv'
+        surveyCSV = 'testdata/AthenaDataExport test.csv'
+        outputCSV = 'testdata/test_convertSalesforceToSFMR.csv'
+        questions, headers = sd.convertSalesforceToSFMR(surveyCSV, datadictCSV)
+        sd.toCSV(outputCSV, questions, headers)
+        # now read from the file and verify
+        del sd
+        sd = convert.SurveyData()
+        sd.data, sd.headers = sd.getData(outputCSV)
+        self.assertEqual(sd.data,[['123456789', 'IDXR123467891376', '+fVQO/nZB7ZEySUzAK5eU8LT88HuJr1vINRQnufsEtM=', 'JOHN', 'STEINBECK', '5/10/12', '9:15 AM', '5/29/12', '1', '196', '1', '0', '0', '3', '0', '0', '1', '1']])
+        remove(outputCSV)
+
+        # should raise an exception for an immutable sequence
+        # self.assertRaises(TypeError, random.shuffle, (1,2,3))
+
+if __name__ == '__main__':
+    unittest.main()
